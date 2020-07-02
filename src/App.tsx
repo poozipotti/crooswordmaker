19354be51375c3944ad8026544e383457a7c62ef
Tessie Urtubia <gittessie@users.noreply.github.com>
2020-07-01 23:57:23 -0400

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import React, {useState} from 'react';
-import styled from 'styled-components'
-import {Editor} from './components/Crossword/Editor';
-import {Solver} from './components/Crossword/Solver';
+import React from "react";
+import styled from "styled-components";
+import { Editor } from "./components/Crossword/Editor";
+import { Solver } from "./components/Crossword/Solver";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 
 const AppContainer = styled.div`
-    margin:0;
-    padding:0;
-    width: 100vw;
-    height: 100vh;
-`
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+`;
 
 function App() {
-  const [isEditing, setIsEditing] = useState<boolean>(true);
   return (
-    <AppContainer>
-      {isEditing ? <Editor/> : <Solver/>}
-    <button onClick={(): void => setIsEditing(!isEditing)}>Click to {isEditing? "solve" : "create"} a puzzle!</button>
-    </AppContainer>
+    <Router>
+      <AppContainer>
+        <Link to="/edit">Editor</Link><br/>
+        <Link to="/solve">Solver</Link>
+        <Switch>
+          <Redirect exact from="/" to="/edit" />
+          <Route path="/edit">
+            <Editor/>
+          </Route>
+          <Route path="/solve">
+            <Solver/>
+          </Route>
+        </Switch>
+      </AppContainer>
+    </Router>
   );
 }
 
